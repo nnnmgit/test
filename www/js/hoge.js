95e17a4858331522a68b20e7416c493318641202
nanonum <contact@nanonum.com>
2015-05-31 21:03:54 +0900

    こわれてる

---
+++ www/js/hoge.js
@@ -0,0 +1,14 @@
+
+$(function(){
+
+	var hogehoge = document.getElementById('neko');
+	$('div')
+		.css('background', '#299')
+		.css('color', '#ff3')
+
+	$('.button').click(function(){
+		alert("CLICK")
+	})
+})
+
+var TEST = 3;
